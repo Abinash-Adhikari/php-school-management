9c1d49560f40c34858650fd1abc21781418db8b2
Abinash-Adhikari <abinashadhikari48@gmail.com>
2022-11-29 22:35:46 +0545

    dev-aninash:operation file changes

--- operation.php
+++ operation.php
@@ -2,17 +2,22 @@
     include("./includes/dbConnection.php");
     include("./functions/php/abinash_function.php");
 <<<<<<< HEAD
+<<<<<<< HEAD
     include("./functions/php/abinash_database_function.php");
     // include("./includes/header.php");
 =======
     include("./includes/header.php");
 >>>>>>> 5b8a742 (dev-aninash:operation file changes)
+=======
+    include("./includes/header.php");
+>>>>>>> 11721d5 (dev-aninash:operation file changes)
     if(isset($_GET['module'])){
         $module=$_GET['module'];
         $createFile='';
         $folder='./systemAdmin/modules/'.$module;
         if(isset($_GET['page'])){
             $page=$_GET['page'];
+<<<<<<< HEAD
 <<<<<<< HEAD
             $operation='./systemAdmin/modules/'.$module.'/operation'.'/'.$page.'.php';
             $include='./systemAdmin/modules/'.$module.'/'.$page.'.php';
@@ -22,6 +27,8 @@
         }
         include($operation);
 =======
+=======
+>>>>>>> 11721d5 (dev-aninash:operation file changes)
             $operation='./systemAdmin/modules/'.$module.'/operation'.'/'.$page.'_operation.php';
             $include='./systemAdmin/modules/'.$module.'/'.$page.'.php';
         }else{
@@ -29,7 +36,10 @@
             $include='./systemAdmin/modules/'.$module.'/'.$module.'.php';
         }
         include($include);
+<<<<<<< HEAD
 >>>>>>> 5b8a742 (dev-aninash:operation file changes)
+=======
+>>>>>>> 11721d5 (dev-aninash:operation file changes)
     }else{
         echo '<script>
                 alert("Page Not Found"); 
