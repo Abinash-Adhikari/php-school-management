2ba03ac23e2ff115bd6ca58d3602be3a1921aef7
Abinash-Adhikari <abinashadhikari48@gmail.com>
2022-11-30 00:31:36 +0545

    dev-abinash: Operation file changes add make dd,ddd function

--- operation.php
+++ operation.php
@@ -1,20 +1,21 @@
 <?php 
     include("./includes/dbConnection.php");
     include("./functions/php/abinash_function.php");
-    include("./includes/header.php");
+    include("./functions/php/abinash_database_function.php");
+    // include("./includes/header.php");
     if(isset($_GET['module'])){
         $module=$_GET['module'];
         $createFile='';
         $folder='./systemAdmin/modules/'.$module;
         if(isset($_GET['page'])){
             $page=$_GET['page'];
-            $operation='./systemAdmin/modules/'.$module.'/operation'.'/'.$page.'_operation.php';
+            $operation='./systemAdmin/modules/'.$module.'/operation'.'/'.$page.'.php';
             $include='./systemAdmin/modules/'.$module.'/'.$page.'.php';
         }else{
-            $operation='./systemAdmin/modules/'.$module.'/operation'.'/'.$module.'_operation.php';
+            $operation='./systemAdmin/modules/'.$module.'/operation'.'/'.$module.'.php';
             $include='./systemAdmin/modules/'.$module.'/'.$module.'.php';
         }
-        include($include);
+        include($operation);
     }else{
         echo '<script>
                 alert("Page Not Found"); 
